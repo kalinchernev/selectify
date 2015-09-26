178bf50d44b6496304408494053f087df316243c
Kalin Chernev <kalin.chernev@gmail.com>
2015-09-26 23:29:48 +0200

    Adding on/off buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,12 +8,30 @@
       item: 'selectify__option',
       other: '.selectify__other'
     });
-
+    $("[name='dropdown-switcher']").bootstrapSwitch({
+      onSwitchChange: function(event, state) {
+        state ? list.trigger('show.dropdown') : list.trigger('hide.dropdown');
+      }
+    });
+    $("[name='list-switcher']").bootstrapSwitch({
+      onSwitchChange: function(event, state) {
+        state ? list.trigger('show.list') : list.trigger('hide.list');
+      }
+    });
     $('.trigger').on('click', function(){
       var button = $(this).data();
       switch (button.action) {
-        case 'hide-list':
-          button.trigger('show.dropdown');
+        case 'show.dropdown':
+          list.trigger('show.dropdown');
+          break;
+        case 'hide.list':
+          list.trigger('hide.list');
+          break;
+        case 'show.list':
+          list.trigger('show.list');
+          break;
+        case 'hide.dropdown':
+          list.trigger('hide.dropdown');
           break;
         default :
           break;
